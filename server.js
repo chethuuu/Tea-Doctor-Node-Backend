139d8eb42c419ab725633d547cbcc23ecfe0f58b
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2023-09-05 20:24:35 +0530

    fix disease model issues

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const PORT = 8091;
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+// const userRoute = require("./app/routers/user.router");
+const detectionRoute = require("./app/routers/detection.router");
+
+const userRoute = require("./app/routers/user.router");
+const partnerRoute = require("./app/routers/partner.router");
+
+// nodemon start
+// const fertilizerRoute = require("./app/routers/fertilizer.router");
+const staffRoute = require("./app/routers/staff.router");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    (err) => {
+      console.log("Can not connect to the database" + err);
+    }
+  );
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use("/user", userRoute);
+app.use("/detection", detectionRoute);
+app.use("/partner", partnerRoute);
+app.use("/user", userRoute);
+
+// app.use("/fertilizer-detection", fertilizerRoute);
+app.use("/staff", staffRoute);
+
+// Create a Server
+const server = app.listen(PORT, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+});
